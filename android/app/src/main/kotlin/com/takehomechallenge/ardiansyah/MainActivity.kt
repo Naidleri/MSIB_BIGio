ce089261dd3cac94261577bfbf05b8c9bf1613ad
haris ardian <harisfirman387@gmail.com>
2024-08-20 12:30:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/takehomechallenge/ardiansyah/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.takehomechallenge.ardiansyah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:msibbigio/view/homepage.dart';
+import 'package:provider/provider.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  runApp(
+    MultiProvider(
+      providers: [
+    
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rick and Morty',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Homepage(),
+    );
+  }
+}
