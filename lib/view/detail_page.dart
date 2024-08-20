fb51778fd8353b2ac5051879883924ff3618d39f
haris ardian <harisfirman387@gmail.com>
2024-08-21 05:55:09 +0700

    refactor : detail page fetching api character

--- lib/view/detail_page.dart
+++ lib/view/detail_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:msibbigio/widget/character_detail.dart';
+import 'package:provider/provider.dart';
+import '../models/character.dart';
 
 class CharacterDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final character = ModalRoute.of(context)!.settings.arguments as Character;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('character fav'),
+        title: Text(character.name),
         actions: [
           IconButton(
             icon: Icon(Icons.favorite),
@@ -14,7 +18,7 @@ class CharacterDetailPage extends StatelessWidget {
           ),
         ],
       ),
-      body: CharacterDetail(),
+      body: CharacterDetail(character: character),
     );
   }
 }
--- lib/widget/character_detail.dart
+++ lib/widget/character_detail.dart
@@ -1,31 +1,29 @@
 import 'package:flutter/material.dart';
+import '../models/character.dart';
 
 class CharacterDetail extends StatelessWidget {
+  final Character character;
+
+  CharacterDetail({required this.character});
+
   @override
   Widget build(BuildContext context) {
-    final String name = 'Character 1';
-    final String species = 'Species 1';
-    final String gender = 'Gender 1';
-    final String origin = 'Origin 1';
-    final String location = 'Location 1';
-    final String imageUrl = 'https://via.placeholder.com/150';
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(name),
+        title: Text(character.name),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network(imageUrl),
+            Image.network(character.imageUrl),
             SizedBox(height: 16),
-            Text('Name: $name', style: TextStyle(fontSize: 18)),
-            Text('Species: $species', style: TextStyle(fontSize: 18)),
-            Text('Gender: $gender', style: TextStyle(fontSize: 18)),
-            Text('Origin: $origin', style: TextStyle(fontSize: 18)),
-            Text('Location: $location', style: TextStyle(fontSize: 18)),
+            Text('Name: ${character.name}', style: TextStyle(fontSize: 18)),
+            Text('Species: ${character.species}', style: TextStyle(fontSize: 18)),
+            Text('Gender: ${character.gender}', style: TextStyle(fontSize: 18)),
+            Text('Origin: ${character.originName}', style: TextStyle(fontSize: 18)),
+            Text('Location: ${character.locationName}', style: TextStyle(fontSize: 18)),
           ],
         ),
       ),
